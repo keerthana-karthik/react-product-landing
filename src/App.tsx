4400295b0e025938af02cd84fd05afca78c1ea47
Keerthana <keerthana@Karthiks-MacBook-Pro.local>
2020-11-06 16:05:44 -0800

    Format our code whenever we make a commit in git

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className={styles["Bg-Blue"]}>
-      Test1
+    <div className={
+    styles["Bg-Blue"]}>
+      Testing Husky
     </div>
   );
 }
